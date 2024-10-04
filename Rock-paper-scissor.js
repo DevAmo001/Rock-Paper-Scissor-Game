1c6f27dd400bf80dc591c39820f7096f2b2cd0d4
Amos Kipruto <kiprutoamos44@gmail.com>
2024-10-04 23:32:39 +0300

    moved the files to another folder

--- Rock-paper-scissor.js
+++ Rock-paper-scissor.js
@@ -16,6 +16,26 @@ if (!score) {
 }
 */
 
+// Auto play button interactivity.
+let isAutoPlaying = false;
+let intervalId;
+
+function autoPlay() {
+  if (!isAutoPlaying) {
+    intervalId = setInterval(function() {
+      const playerMove = pickComputerMove();
+      playGame(playerMove);
+    }, 1000);
+
+    isAutoPlaying = true;
+
+  } else {
+    clearInterval(intervalId);
+    isAutoPlaying = false;
+  }
+  
+}
+
 // comparing our moves with the computer moves and update the results.
 function playGame(playerMove) {
   const computerMove = pickComputerMove();
