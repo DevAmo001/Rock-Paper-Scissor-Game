96b2a4970a7d0a217c193221147e10b77db985eb
Amos Kipruto <kiprutoamos44@gmail.com>
2024-10-08 17:56:31 +0300

    renamed and changed files directory.

--- index.js
+++ index.js
@@ -88,8 +88,8 @@ function playGame(playerMove) {
 
   document.querySelector('.js-moves').innerHTML = 
   `You 
-<img src="../Emojis/${playerMove}-emoji.png" class="move-icon">
-<img src="../Emojis/${computerMove}-emoji.png" class="move-icon">
+<img src="${playerMove}-emoji.png" class="move-icon">
+<img src="${computerMove}-emoji.png" class="move-icon">
 Computer`;
 }
 
